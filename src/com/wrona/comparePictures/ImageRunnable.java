b9fb4382bdf1990cc25837b336c07e61a115518b
przemyslawwrona20 <przemyslawwrona20@outlook.com>
2015-03-29 12:32:06 +0200

    Refactor classes

---
+++ src/com/wrona/comparePictures/ImageRunnable.java
@@ -0,0 +1,84 @@
+package com.wrona.comparePictures;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+public class ImageRunnable implements Runnable {
+	BufferedImage imageFirst, imageSecond, imageFinal;
+
+	public ImageRunnable(BufferedImage imageFirst, BufferedImage imageSecond) {
+		super();
+		this.imageFirst = imageFirst;
+		this.imageSecond = imageSecond;
+	}
+
+	@Override
+	public void run() {
+		int height, weight;
+		if (imageFirst.getHeight() > imageSecond.getHeight())
+			height = imageFirst.getHeight();
+		else
+			height = imageSecond.getHeight();
+
+		if (imageFirst.getWidth() > imageSecond.getWidth())
+			weight = imageFirst.getWidth();
+		else
+			weight = imageSecond.getWidth();
+
+		imageFinal = new BufferedImage(weight, height,
+				BufferedImage.TYPE_INT_ARGB);
+
+		WritableRaster rasterFirst = imageFirst.getRaster();
+		WritableRaster rasterSecond = imageSecond.getRaster();
+		WritableRaster rasterFinal = imageFinal.getRaster();
+
+		for (int i = 0; i < weight; i++) {
+			for (int j = 0; j < height; j++) {
+				int sampleFirst[] = new int[4];
+				int sampleSecond[] = new int[4];
+				try {
+					rasterFirst.getPixel(i, j, sampleFirst);
+				} catch (ArrayIndexOutOfBoundsException ex) {
+					sampleFirst[0] = 0;
+					sampleFirst[1] = 0;
+					sampleFirst[2] = 0;
+					sampleFirst[3] = 0;
+				}
+				try {
+					rasterSecond.getPixel(i, j, sampleSecond);
+				} catch (ArrayIndexOutOfBoundsException ex) {
+					sampleSecond[0] = 0;
+					sampleSecond[1] = 0;
+					sampleSecond[2] = 0;
+					sampleSecond[3] = 0;
+				}
+
+				int[] newPixel = comparePixels(sampleFirst, sampleSecond);
+				rasterFinal.setPixel(i, j, newPixel);
+			}
+		}
+		new FramePreview(imageFinal, "FINAL IMAGE");
+	}
+
+	private int[] comparePixels(int sampleFirst[], int sampleSecond[]) {
+		double sum = Math.sqrt(Math.pow(sampleFirst[0] - sampleSecond[0], 2)
+				+ Math.pow(sampleFirst[1] - sampleSecond[1], 2)
+				+ Math.pow(sampleFirst[2] - sampleSecond[2], 2));
+
+		double sumMax = Math.sqrt(3 * 255 * 255);
+		int result = (int) ((1 - sum / sumMax) * 255);
+
+		int[] newPixel = new int[4];
+		newPixel[0] = result;
+		newPixel[1] = result;
+		newPixel[2] = result;
+		newPixel[3] = 255;
+
+		return newPixel;
+	}
+
+	public BufferedImage getImageFinal() {
+		return imageFinal;
+	}
+
+}
---
+++ src/com/wrona/comparePictures/MenuCompare.java
@@ -0,0 +1,159 @@
+package com.wrona.comparePictures;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+@SuppressWarnings("serial")
+public class MenuCompare extends JMenu {
+	public static final int FIRST_PICTURE = 1, SECOND_PICTURE = 2;
+	private BufferedImage imageFirst, imageSecond, imageFinal;
+
+	public MenuCompare(String string) {
+		super(string);
+	}
+
+	/**
+	 * Open image file
+	 * 
+	 * @param value
+	 *            File's path
+	 */
+
+	public void openFile(int value) {
+		JFileChooser chooser = new JFileChooser();
+		
+		
+		//UWAGA TUTAJ
+		chooser.setCurrentDirectory(new File("C:/Users/Student/Desktop"));
+		String extensions[] = ImageIO.getReaderFileSuffixes();
+		chooser.setFileFilter(new FileNameExtensionFilter("Image files",
+				extensions));
+
+		int resultChooser = chooser.showOpenDialog(this);
+		if (resultChooser != JFileChooser.APPROVE_OPTION)
+			return;
+
+		File file = chooser.getSelectedFile();
+		BufferedImage bufferedImage = null;
+
+		try {
+			bufferedImage = ImageIO.read(file);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e);
+		}
+
+		if (value == MenuCompare.FIRST_PICTURE) {
+			imageFirst = bufferedImage;
+			new FramePreview(bufferedImage, "Preview first image");
+		}
+
+		else if (value == MenuCompare.SECOND_PICTURE) {
+			imageSecond = bufferedImage;
+			new FramePreview(bufferedImage, "Preview second image");
+		}
+	}
+
+	/**
+	 * Compare two pictures if pixel is the same on both pictures the results
+	 * color is white if pixel is completely different on both picture the
+	 * results color is black otherwise use grayscale
+	 * 
+	 */
+	public void compare() {
+		if (imageFirst == null && imageSecond == null) {
+			JOptionPane.showMessageDialog(null, "Select pictures");
+			return;
+		} else if (imageFirst == null) {
+			JOptionPane.showMessageDialog(null, "Select first picture");
+			return;
+
+		} else if (imageSecond == null) {
+			JOptionPane.showMessageDialog(null, "Select second picture");
+			return;
+
+		} else {
+			ImageRunnable comparePictures = new ImageRunnable(
+					imageFirst, imageSecond);
+			comparePictures.run();
+			imageFinal = comparePictures.getImageFinal();
+		}
+
+	}
+
+	/**
+	 * Save actual picture in file.
+	 * 
+	 * @param formatName
+	 *            file's format
+	 */
+	public void saveFile(final String formatName) {
+		if (imageFinal == null)
+			return;
+		Iterator<ImageWriter> iter = ImageIO
+				.getImageWritersByFormatName(formatName);
+		ImageWriter writer = iter.next();
+		JFileChooser chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new File("."));
+		String[] extensions = writer.getOriginatingProvider().getFileSuffixes();
+		chooser.setFileFilter(new FileNameExtensionFilter("Image files",
+				extensions));
+		int r = chooser.showSaveDialog(this);
+		if (r != JFileChooser.APPROVE_OPTION)
+			return;
+		File filePath = chooser.getSelectedFile();
+		String filePathString = filePath.getAbsolutePath();
+		filePathString = filePathString + "." + formatName;
+		File f = new File(filePathString);
+
+		try {
+			ImageOutputStream imageOut = ImageIO.createImageOutputStream(f);
+			writer.setOutput(imageOut);
+			writer.write(new IIOImage(imageFinal, null, null));
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e);
+		}
+	}
+
+	/**
+	 * Make a set of preferred name of formats' image file
+	 *
+	 * @return set of names formats
+	 */
+	public static Set<String> getWriterFormats() {
+		Set<String> writerFormats = new TreeSet<>();
+		Set<String> formatNames = new TreeSet<>(Arrays.asList(ImageIO
+				.getWriterFormatNames()));
+		while (formatNames.size() > 0) {
+			String name = formatNames.iterator().next();
+			Iterator<ImageWriter> iter = ImageIO
+					.getImageWritersByFormatName(name);
+			ImageWriter writer = iter.next();
+
+			String[] names = writer.getOriginatingProvider().getFormatNames();
+			String format = names[0];
+
+			if (format.equals(format.toLowerCase()))
+				format = format.toUpperCase();
+
+			writerFormats.add(format);
+			formatNames.removeAll(Arrays.asList(names));
+		}
+		return writerFormats;
+	}
+
+}
